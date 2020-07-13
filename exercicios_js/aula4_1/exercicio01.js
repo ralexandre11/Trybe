c8e3e731c9da746df4e50d4f710ad242c6509af0
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-07-13 18:07:26 -0300

    conclusão do exercicio 01

---
+++ exercicios_js/aula4_1/exercicio01.js
@@ -0,0 +1,14 @@
+let num1 = 10;
+let num2 = 2;
+
+let sum = num1+num2;
+
+let sub = num1 - num2;
+
+let mul = num1 * num2;
+
+let div = num1 / num2;
+
+let mod = num1 % num2;
+
+console.log(sum, sub, mul, div, mod);
