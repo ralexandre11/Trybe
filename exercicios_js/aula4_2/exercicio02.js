e416c4516fe55b715850c2bad7d81897e039b099
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-07-14 17:41:00 -0300

    conclusão do exercicio 02

--- exercicios_js/aula4_2/exercicio02.js
+++ exercicios_js/aula4_2/exercicio02.js
@@ -1,5 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+let soma = 0;
 for (let item = 0; item < numbers.length; item += 1){
-    console.log("Item do array: " + numbers[item]);
+    soma += numbers[item];
 }
+console.log("Soma total: " + soma);
