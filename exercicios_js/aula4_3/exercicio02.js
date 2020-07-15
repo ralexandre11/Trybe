38090aed51e57a69b559e989a6e8c0f07bbfc570
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-07-15 17:43:05 -0300

    Resolução do exercicio 01, 02 e 03

--- exercicios_js/aula4_3/exercicio02.js
+++ exercicios_js/aula4_3/exercicio02.js
@@ -1,9 +1,7 @@
 let n = 5;
 console.log("n = " + n);
+let linha = "";
 for (let i=1; i<=n; i+=1){
-    let linha = "";
-    for (let x=1; x<=n; x+=1){
-        linha = linha + "*";
-    }
+    linha = linha + "*";
     console.log(linha);
 }
--- exercicios_js/aula4_3/exercicio03.js
+++ exercicios_js/aula4_3/exercicio03.js
@@ -2,7 +2,10 @@ let n = 5;
 console.log("n = " + n);
 for (let i=1; i<=n; i+=1){
     let linha = "";
-    for (let x=1; x<=n; x+=1){
+    for (let x=n-i; x>0; x-=1){    
+        linha = linha + " ";
+    }
+    for (let y=1; y<=i; y+=1){    
         linha = linha + "*";
     }
     console.log(linha);
