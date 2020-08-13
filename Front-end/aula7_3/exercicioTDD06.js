44dd4ce48ad822d6922a5b36afca35815ebe0e3d
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-08-13 10:23:47 -0300

    resolucao dos exercicios TDD 05 e 06

---
+++ Front-end/aula7_3/exercicioTDD06.js
@@ -0,0 +1,28 @@
+
+const assert = require('assert');
+// Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+const removeVowels = (word) => {
+  const characters = word.split('');
+  let results = '';
+  let count = 1;
+  for (let i = 0; i < characters.length; i += 1) {
+    if (
+      characters[i] === 'a' ||
+      characters[i] === 'o' ||
+      characters[i] === 'i' ||
+      characters[i] === 'e' ||
+      characters[i] === 'u'
+    ) {
+      results += count;
+      count += 1;
+    } else {
+      results += characters[i];
+    }
+  }
+  return results;
+};
+
+const parameter = 'Dayane';
+const result = 'D1y2n3';
+const output = removeVowels(parameter);
+assert.strictEqual(output, result);
