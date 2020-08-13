da4c6450c04bdb60560bbc7ddb4eb9068f5a595c
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-08-13 10:44:52 -0300

    resolucao dos exercicios TDD 07 e 08

--- Front-end/aula7_3/exercicioTDD-Ref02.js
+++ Front-end/aula7_3/exercicioTDD-Ref02.js
@@ -1,4 +1,3 @@
-
 const assert = require('assert');
 // Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 const removeVowels = (word) => {
